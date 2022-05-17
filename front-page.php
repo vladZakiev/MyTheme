1731175969b7dc33efbbeb2137f1ccb40b317806
vladZakiev <90211959+vladZakiev@users.noreply.github.com>
2022-05-17 23:00:41 +0500

    dinamic vesrsion

--- front-page.php
+++ front-page.php
@@ -9,12 +9,7 @@
     <meta name="description" content="Blog Site Template">
     <meta name="author" content="https://youtube.com/FollowAndrew">    
     <link rel="shortcut icon" href="images/logo.png"> 
-    
-    <!-- FontAwesome CSS-->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-	<!-- Bootstrap CSS-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">  
-	<!-- Theme CSS -->
+
 	<?php 
         wp_head();
     ?>
--- functions.php
+++ functions.php
@@ -1,6 +1,9 @@
 <?php 
     function theme_register_styles() { 
-        wp_enqueue_style('getStyles', get_template_directory_uri () . '/style.css' , array(), '1.0.0', 'all' );
+        $version = wp_get_theme () -> get('Version');
+        wp_enqueue_style ('my-theme-style', get_template_directory_uri () . '/style.css' , array('my-theme-bootstrap'), $version, 'all' );
+        wp_enqueue_style ('my-theme-bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css' , array(), '4.4.1', 'all' );
+        wp_enqueue_style ('my-theme-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css' , array(), '5.13.0', 'all' );
     }
 
     add_action( 'wp_enqueue_scripts', 'theme_register_styles' );
